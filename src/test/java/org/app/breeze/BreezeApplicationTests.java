099af0b95a1a8b3f6c4808a38b2c7cb248945162
MECTRON <saab3500@gmail.com>
2024-10-16 19:23:34 +0300

    initial commit

---
+++ src/test/java/org/app/breeze/BreezeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.breeze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreezeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
