e21ad401a4efc27adb85b41140c3e6fb03cba3d1
MECTRON <saab3500@gmail.com>
2024-10-18 00:09:48 +0300

    Добавлен Swagger

--- src/main/java/org/app/breeze/config/WebConfig.java
+++ src/main/java/org/app/breeze/config/WebConfig.java
@@ -1,4 +1,4 @@
-package org.app.breeze;
+package org.app.breeze.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
